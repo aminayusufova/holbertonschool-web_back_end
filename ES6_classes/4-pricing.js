39e678aa558b55a9f1dc5836cacb3c678c7df1a0
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-07-19 18:50:06 +0400

    commited

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,34 +1,48 @@
-import { Currency } from './3-currency.js'
+import Currency from './3-currency';
 
-class Pricing {
-	_amount
-	_currency
-	constructor(amount, currency) {
-		this._amount = amount;
-		this._currency = currency;
-	}
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-	set _amount(x) {
-		this._amount = amount;
-	}
+  get amount() {
+    return this._amount;
+  }
 
-	get _amount() {
-		return this._amount;
-	}
+  get currency() {
+    return this._currency;
+  }
 
-	set _currency(x) {
-		this._currency = currency;
-	}
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
 
-	get _currency() {
-		return this._currency;
-	}
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = currency;
+  }
 
-	displayFullPrice() {
-		return this._amount this.currency (Currency.code);
-	}
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
 
-	static convertPrice(amount: Number, conversionRate: Number) {
-		return amount * conversionRate;
-	}
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+    return amount * conversionRate;
+  }
 }
