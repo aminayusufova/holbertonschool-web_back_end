9ccb8e9e22f790e688d78f4bc694d695041bd147
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-07-19 18:47:41 +0400

    commited

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,23 +1,26 @@
 export default class Currency {
-  _code;
-  _name;
   constructor(code, name) {
     this._code = code;
     this._name = name;
   }
+
   set code(x) {
     this._code = x;
   }
+
   get code() {
     return this._code;
   }
+
   set name(x) {
-    this._name = name;
+    this._name = x;
   }
+
   get name() {
     return this._name;
   }
+
   displayFullCurrency() {
-    return this._name+" ("+this._code+")";
+    return `${this._name} (${this._code})`;
   }
 }
