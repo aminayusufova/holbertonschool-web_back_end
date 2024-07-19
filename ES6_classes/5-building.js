86f06ba4531bbc3b4db86021e1717625d09f26e5
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-07-19 18:51:39 +0400

    commited

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building
+            && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square feet must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
