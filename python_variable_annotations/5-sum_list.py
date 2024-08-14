fe7e2b734315fd979470323f91cf63fe2a9eee37
aminayusufova <158808697+aminayusufova@users.noreply.github.com>
2024-08-14 17:31:08 +0400

    commited

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''yes yes yo'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''adds all floats of input list'''
+    return sum(input_list)
